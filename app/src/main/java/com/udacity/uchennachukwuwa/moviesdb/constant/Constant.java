17812511ab63377f2b4169bf3668c29ff6eb689f
Dablaze-ufc <chukwuwauchena@gmail.com>
2020-07-05 22:50:06 +0100

    added the details Activity

--- app/src/main/java/com/udacity/uchennachukwuwa/moviesdb/constant/Constant.java
+++ app/src/main/java/com/udacity/uchennachukwuwa/moviesdb/constant/Constant.java
@@ -2,7 +2,7 @@ package com.udacity.uchennachukwuwa.moviesdb.constant;
 
 public class Constant {
     public static String BASE_URL = "https://api.themoviedb.org/3/";
-    public static String API_KEY  = "25da776002b035d134638b59da779f77";
+    public static String API_KEY  = "";
     public static String POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342";
     public static String INTENT_KEY = "movie_item";
 }
